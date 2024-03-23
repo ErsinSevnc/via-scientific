1fd4b00e6a4b73125f59ee46b5d837c75b4ee03e
Ersin Sevinç <ersin.sevinc@venhancer.com>
2024-03-23 22:18:56 +0300

    feat: statistics calculations during seed operation

--- server/utils/seed-db.js
+++ server/utils/seed-db.js
@@ -1,11 +1,26 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const GeneExpressions = require('../models/GeneExpression');
+const mathjs = require('mathjs');
 
 async function seedDb() {
-    const geneExpressions = fs.readFileSync('./data/gene_expression_data.json', 'utf8');
-    const data = JSON.parse(geneExpressions);
-    await GeneExpressions.insertMany(data);
+    try {
+        const geneExpressions = fs.readFileSync('./data/gene_expression_data.json', 'utf8');
+        const data = JSON.parse(geneExpressions);
+        const transformedData = data.map(geneExpression => {
+            return {
+                ...geneExpression,
+                variance: mathjs.variance(geneExpression.expressionValues),
+                mean: mathjs.mean(geneExpression.expressionValues),
+                median: mathjs.median(geneExpression.expressionValues)
+            }
+        })
+
+        await GeneExpressions.insertMany(transformedData);
+
+    } catch (e) {
+        throw new Error(e);
+    }
 };
 
 
