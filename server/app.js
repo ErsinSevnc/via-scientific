da07f141d6d6426a6ddecd2af6862ad6f74cb6c5
Ersin Sevinç <ersin.sevinc@venhancer.com>
2024-03-23 18:25:49 +0300

    feat: app and server.js seperated

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const app = require('express')();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const seedDb = require('./utils/seed-db');
+
+const expressionRoutes = require('./routes/expressions');
+const geneAnalysisRouters = require('./routes/gene-analysis');
+
+app.use(express.json());
+app.use(cors());
+app.use('/expressions', expressionRoutes);
+app.use('/gene-analysis', geneAnalysisRouters);
+
+const DB = process.env.MONGO_URL;
+
+mongoose
+    .connect(DB)
+    .then(() => seedDb())
+    .catch(error => console.log(error));
+
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,25 +1,6 @@
-const app = require('express')();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-
-const seedDb = require('./utils/seed-db');
-
-const expressionRoutes = require('./routes/expressions');
-const geneAnalysisRouters = require('./routes/gene-analysis');
-
-app.use(express.json());
-app.use(cors());
-app.use('/expressions', expressionRoutes);
-app.use('/gene-analysis', geneAnalysisRouters);
+const app = require('./app');
 
 const PORT = process.env.PORT || 8080;
-const DB = process.env.MONGO_URL;
-
-mongoose
-    .connect(DB)
-    .then(() => seedDb())
-    .catch(error => console.log(error));
 
 app.listen(PORT, () => {
     console.log(`Server Listening on port: ${PORT}`)
